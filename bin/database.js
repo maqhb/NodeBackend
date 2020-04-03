8a55ac344d00324b0d263145e76d8816f8ba6ed4
AbdulWaheed <waheed.moeed123@gmail.com>
2020-04-03 22:20:58 +0500

    fixes some issues of db connections

--- bin/database.js
+++ bin/database.js
@@ -2,26 +2,40 @@ const mongoose = require("mongoose");
 
 const winston = require("winston").loggers
 
-function createConnection (){
-    const logger = winston.get("database.js");
+let logger;
+async function createConnection (){
+    logger = winston.get("database.js");
     // Connecting to the database
-    mongoose.connect(process.env.DBURL ,{
-        useNewUrlParser: true
+    return mongoose.connect(process.env.DBURL ,{
+        useNewUrlParser: true,
+        reconnectInterval:5000,
+        bufferMaxEntries:0,
+        poolSize:10,
+        reconnectTries:3,
     }).then(() => {
-        logger.info("Successfully connected to the database\n");    
+        logger.info("Successfully connected to the database\n");
+        registerListener()
+        return true
     }).catch(err => {
         logger.error('Could not connect to the database.\n', err);
-        logger.info("Trying connection again with in 30s\n")
+        return false
     })
+}
+
+function registerListener(){
     mongoose.Promise = global.Promise;
 
-    mongoose.connection.on('error', err => {
+    mongoose.connection.on('error', (err) => {
         logger.error("Database conection dropped, Trying again\n"+err)
     });
 
+    mongoose.connection.on("reconnect", ()=>{
+        logger.info("Reconnected.\n")
+    })
+
     mongoose.connection.on("reconnectFailed", ()=>{
         logger.error("Mongoose has run out of reconnectTries.\n")
-        process.exit(1)
+        process.exit(2)
     })
 }
 
--- bin/server.js
+++ bin/server.js
@@ -1,9 +1,49 @@
 const dotenv = require("dotenv")
 dotenv.config()
+const morgan  = require("morgan")
+const winston_logger = require("winston").loggers
+
+const winston = require("./config/logger")
+const loggercategories = require("./config/loggercategories")
+const database = require("./database")
 const app = require("../index")
 
-const port = (process.env.PORT || 3000)
+//Combinig morgan and winston for logging purpose
+app.use(morgan('combined', { stream: winston.stream }))
+loggercategories.configLoggerCategories()
 
-const server = app.listen(port, ()=>{
-    console.log("Server is listening on >",port)
-})
+const logger = winston_logger.get("server.js")
+//Create connection with mongodb and make that connection globally accessible and also handle failure of
+//connection at first try
+let intialRetry = 0
+configServer()
+
+async function configServer(){
+    if( await database.createConnection()){
+        startServer()
+    }else{
+        intialRetry = intialRetry+1
+        handleReconnection()
+    }
+}
+
+function startServer(){
+    const port = (process.env.PORT || 3000)
+
+    app.listen(port, ()=>{
+        logger.info("Server is listening on >"+port)
+    })
+}
+
+//Try three times to connect after every 30sec, in case of failure exit with 1
+async function handleReconnection(){
+    if(intialRetry <= 3){
+        logger.info("Trying connection again with in 30s\n")
+        setTimeout(()=>{
+            configServer()
+        },30000)
+    }else{
+        logger.error("Failed to connect database, 3 tries completed")
+        process.exit(1)
+    }
+}
