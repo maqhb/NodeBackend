030bb352e8ad0c05356efaca3d4dc3bd1c016021
AbdulWaheed <waheed.moeed123@gmail.com>
2020-04-09 22:16:38 +0500

    Putting andGetting OAuth token functionality added

--- router/auth_router.js
+++ router/auth_router.js
@@ -1,14 +1,31 @@
 const express = require("express");
-const router = express.Router();
-const userController = require("../Controller/UserController");
+
 const withAuth = require("../lib/Auth")
+const userController = require("../Controller/UserController");
+const oAuthTokensController = require("../Controller/OAuthTokensController")
+
+const router = express.Router();
 
 router.post("/authenticateUser/",userController.authenticateUser)
 
 router.post("/createUser/", userController.registerUser)
 
-router.post("/testToken/",withAuth ,(req, res)=>{
-    res.send("Authorization with token is done"+req.email)
-})
+//Setting tokens
+router.post("/fb_token/",withAuth ,oAuthTokensController.setFb_Token)
+
+router.post("/tw_token/",oAuthTokensController.setTw_Token)
+
+router.post("/inst_token/" ,oAuthTokensController.setInst_Token)
+
+router.post("/pin_token/",withAuth ,oAuthTokensController.setPin_Token)
+
+//Getting tokens
+router.get("/fb_token/:userId",withAuth ,oAuthTokensController.getFb_Token)
+
+router.get("/tw_token/:userId/",oAuthTokensController.getTw_Token)
+
+router.get("/inst_token/:userId/" ,oAuthTokensController.getInst_Token)
+
+router.get("/pin_token/:userId",withAuth ,oAuthTokensController.getPin_Token)
 
 module.exports = router
